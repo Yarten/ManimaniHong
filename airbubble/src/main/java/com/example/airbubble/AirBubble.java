5eb6cd63c3777a4204eb0509fefe05df69dcab4d
YSJian97 <1365279357@qq.com>
2018-01-01 22:29:15 +0800

    ysjian

---
+++ airbubble/src/main/java/com/example/airbubble/AirBubble.java
@@ -0,0 +1,45 @@
+package com.example.airbubble;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by lenovo on 2018/1/1.
+ */
+
+public class AirBubble extends ConstraintLayout {
+
+    private ConstraintLayout bubbleLayout;
+    private TextView description;
+    private ImageView triangle;
+
+    public AirBubble(Context context) {
+        this(context,null);
+    }
+
+    public AirBubble(Context context, AttributeSet attributeSet) {
+        super(context,attributeSet);
+        LayoutInflater.from(context).inflate(R.layout.air_bubble,this);
+
+        ini();
+    }
+
+    private void ini() {
+        bubbleLayout = findViewById(R.id.BubbleLayout);
+        description = findViewById(R.id.Description);
+        triangle = findViewById(R.id.Triangle);
+    }
+
+    public void setText(String buttonDescription) {
+        this.description.setText(buttonDescription);
+    }
+
+    public String getText() {
+        return description.getText().toString();
+    }
+}
